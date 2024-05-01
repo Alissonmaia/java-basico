72e97d79aa8d3ad9aa13674f0495398d8f52bb77
Alissonmaia <alissonmaiadasilva28@gmail.com>
2024-05-01 15:48:04 -0300

    Methods in Java

---
+++ class-java/classes-java/methods/methods/README.md
@@ -0,0 +1,19 @@
+# Métodos em Java
+### O que é um Método?
+
+>Um método em Java é equivalente a uma função, subrotina ou procedimento em outras linguagens de programação.
+
+## Estrutura Básica
+
+>TipoRetorno NomeObjetivoNoInfinitivo Parâmetro (s)
+
+>Métodos em Java sempre no infinitivo.
+
+>Um método pode receber parâmetros de tipos diferentes
+
+>EX: int somar (int numeroUm, int numero2);
+
+>Retorno pode ser do mesmo tipo ou diferente. EX: String != long 
+
+>EX: String formatarCep (long cep);
+
---
+++ class-java/classes-java/methods/methods/src/methods.java
@@ -0,0 +1,20 @@
+public class methods {
+    public static void main (String [] args){
+    /** Corpo principal */    
+        String primeiroNome = "Alisson";
+        String segundoNome = "maia";
+
+        /** Variável chamada próprio método com parâmetro */
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        /** Print concatenação nome completo */
+        System.out.println(nomeCompleto);
+
+    }
+
+    /** Inicio do Método retornar um texto*/
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        /** Corpo de retorno método */
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }    
+
+}
