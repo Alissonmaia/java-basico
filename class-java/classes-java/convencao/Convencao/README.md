2947210997f82ca13ac1c56ac471ce0d04840414
Alissonmaia <alissonmaiadasilva28@gmail.com>
2024-05-01 14:54:25 -0300

    Convenção de Codificação em Java

---
+++ class-java/classes-java/convencao/Convencao/README.md
@@ -0,0 +1,25 @@
+# Boas Práticas de Código em Java 🖥️
+
+## A estrutura padrão para se declarar uma variável sempre é:
+
+>Tipo nomeVariavel atribuicaoDeValorOpcional;
+
+>EX: int anoFabricacao = 2000;
+
+## Tipos de Dados
+
+>Os oito tipos primitivos em Java são: int, byte, short, long, float, double, boolean e char.
+
+## Declaração de variáveis válidas
+
+>Deve conter apenas letras, _ (underline), $ ou os números de 0 a 9
+
+>Dever sempre iniciar por uma letra ou _ (underline) ou $, jamais com número
+
+>Deve iniciar com uma letra minúscula
+
+>Não pode conter espaços
+
+>Não pode usar palavras reservadas da linguagem 
+
+>O nome deve ser único dentro de um escopo 
---
+++ class-java/classes-java/convencao/Convencao/src/Convencao.java
@@ -0,0 +1,29 @@
+/** Corpo da Classe */
+public class Convencao {
+
+    /** Corpo do método */
+    /** Parâmetro Argumento (args) Array Tipo String  */
+    public static void main (String [] args) {
+        /** Regra Adicional para variável não mudar seu valor */
+        /** Palavra Reservada <Final>  */
+        final String BR = "Brasil";
+        double PI =3.14;
+        int ESTADOS_BRASIL = 27;
+        /** Não alterar o valor BR */
+        BR = "Brazil";
+        /** Variável ano que alterar seu valor */
+        int ano =2023;
+        ano =2024;
+
+        /** camelCase */
+        int anoFabricacao = 2000;
+        /** Variável imutável */
+        int ANO_2020 =1998;
+
+        /** Declaração válidas de Variáveis */
+        int numero$um =1;
+        int numero1 =1;
+        int numeroum =1;
+        int longo =1;
+    } 
+}
