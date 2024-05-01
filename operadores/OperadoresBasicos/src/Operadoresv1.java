be386f708b52d7dfd36c76089d8038d7c712c008
Alissonmaia <alissonmaiadasilva28@gmail.com>
2024-05-01 20:52:50 -0300

    Operadores Unários em Java

---
+++ operadores/OperadoresBasicos/src/Operadoresv1.java
@@ -0,0 +1,14 @@
+public class Operadoresv1 {
+    public static void main(String[] args) {
+        //operadores unários
+        int numero = 5;
+         numero = - numero;
+        System.out.println(numero);
+        numero = + numero;
+        System.out.println(numero);
+        numero = numero * -1;
+        System.out.println(numero); 
+
+    }
+    
+}
