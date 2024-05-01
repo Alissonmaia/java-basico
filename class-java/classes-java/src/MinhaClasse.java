a4d1c02a54078062f925cb3a437f3377960c8282
Alissonmaia <alissonmaiadasilva28@gmail.com>
2024-05-01 13:41:33 -0300

    Anatomia-classes-Java

---
+++ class-java/classes-java/src/MinhaClasse.java
@@ -0,0 +1,10 @@
+/** Corpo da Classe */
+public class MinhaClasse {
+
+    /*Corpo do metódo */
+    /** Parametro Argumentos Array Tipo String  */
+    public static void main (String [] args) {
+
+    System.out.print("Definindo uma classe e um metódo");    
+    } 
+}
